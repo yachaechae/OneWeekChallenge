3061163907ca0dc66266b590752a08fce68c37ed
yachaechae <wpql1207@gmail.com>
2024-07-09 18:52:54 +0900

    [Day2] router-dom 설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
 import "./styles/index.css";
 import "./styles/custom.css";
 import Home from "./pages/Home.tsx";
 import Todos from "./pages/Todos.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-// import "./styles/custom.css";
+import "./styles/custom.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Register from "./pages/Register.tsx";
+import Login from "./pages/Login.tsx";
+import MyPage from "./pages/MyPage.tsx";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<React.StrictMode>
-		<QueryClientProvider client={queryClient}>
-			{/* <App />
-			<Home /> */}
-			<Todos />
-		</QueryClientProvider>
+		<BrowserRouter>
+			<QueryClientProvider client={queryClient}>
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/todo" element={<Todos />} />
+					<Route path="/login" element={<Login />} />
+					<Route path="/register" element={<Register />} />
+					<Route path="/mypage" element={<MyPage />} />
+				</Routes>
+			</QueryClientProvider>
+		</BrowserRouter>
 	</React.StrictMode>
 );
