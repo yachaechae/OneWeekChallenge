c31d55b2431c4e6b90996da4d9c5494b544b1e89
yachaechae <wpql1207@gmail.com>
2024-07-09 10:54:46 +0900

    [Day2] env 설정에 따른 수정

--- src/lib/axiosInstance.ts
+++ src/lib/axiosInstance.ts
@@ -1,6 +1,5 @@
 import axios from "axios";
 
 export const instance = axios.create({
-	// baseURL: import.meta.env.VITE_AXIOS_BASE_URL as string,
-	baseURL: `https://jsonplaceholder.typicode.com`,
+	baseURL: import.meta.env.VITE_AXIOS_BASE_URL as string,
 });
